0fcff63fd0ae2877ac2b25be26be08ba4832ba97
Ivan Cottiero <ic47299@fer.hr>
2017-11-08 14:02:11 +0100

    Clean project added

--- Firma/MainPage.xaml.cs
+++ Firma/MainPage.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -24,27 +22,9 @@ namespace Firma
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        ObservableCollection<FirmaDAL.Artikl> Artikli { get; } = new ObservableCollection<FirmaDAL.Artikl>();
-
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            var result = await Load();
-            foreach (var item in result)
-            {
-                Artikli.Add(item);
-            }
-        }
-
-        public async static Task<List<FirmaDAL.Artikl>> Load()
-        {
-            var dalProvider = new FirmaDAL.ArtiklDalProvider();
-            var result = await Task.Run(() => dalProvider.FetchAll());
-            return result;
         }
     }
 }
